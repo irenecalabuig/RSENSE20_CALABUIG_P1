a5f12b4635a1525b1c2f98899c19637a1e5ae1d9
Irene Calabuig Adobes <719364@unizar.es>
2020-10-31 08:31:37 +0100

    Genero una salida PWM a 5kHz proporcional a la lectura ADC. [TERMINADA]

--- 3.pwm/pwm/pwm.ino
+++ 3.pwm/pwm/pwm.ino
@@ -1,27 +1,32 @@
-// the number of the LED pin
-const int ledPin = 12;  // 12 corresponds to GPIO12 SACO LA SALIDA PWM
+//---------------------------------------- PRÁCTICA 1 ----------------------------------------//
+// Ejercicio 3: Generar una salida PWM a 5KHz proporcional a la lectura del ADC
+
+const int SalidaPWM = 12;  // 12 corresponds to GPIO12 SACO LA SALIDA PWM
+const int pinPotenciometro = 2; //Pin del potenciómetro
+
 int y; //salida ajustada
+int ValorPotenciometro; //Variable que almacena el valor del potenciómetro
 
 // Características PWM
 const int freq = 5000;// (5KHz)
-const int pinPotenciometro = 2; //Pin del potenciómetro
 const int resolution = 8; //(2^8)-1=255
  
 void setup(){
   // configuracion PWM
-  ledcSetup(pinPotenciometro, freq, resolution);
-  
-  // attach the channel to the GPIO to be controlled
-  ledcAttachPin(ledPin, pinPotenciometro); //de donde obtener la señal, canal donde genero señal 
+  ledcSetup(pinPotenciometro, freq, resolution); 
+  //vinculo el canal de la GPIO ("SalidaPWM") para ser controlado por el potenciómetro
+  ledcAttachPin(SalidaPWM, pinPotenciometro); //(Donde obtengo la salida, de donde modulo el PWM)
 }
  
 void loop(){
-  int ValorPotenciometro = analogRead(pinPotenciometro); //Lee la entrada analógica del puerto 2
-  y = map(ValorPotenciometro, 0, 4095 , 0, 255); //Reajusto los valores
-  ledcWrite(pinPotenciometro,y);
+  ValorPotenciometro = analogRead(pinPotenciometro); //Leo entrada analógica del potenciómetro
+  y = map(ValorPotenciometro, 0, 4095 , 0, 255); //map(value, fromLow, fromHigh, toLow, toHigh)
+  // He ajustado el valor de salida de 0 a 255 (resolución máxima)
+  ledcWrite(pinPotenciometro,y); //(canal, dutty)
   delay(20);
  
 }   
+
 
 
   
