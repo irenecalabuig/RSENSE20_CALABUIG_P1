f3d444f7e53ba18ef0dc0f3bf176e4c8b1c271a7
Irene Calabuig Adobes <719364@unizar.es>
2020-10-31 08:29:33 +0100

    Leer el valor del ADC y mostrarlo por pantalla con periocidad de 1 segundo [TERMINADA]

--- 1.leer_valor_ADC/leer_valor_ADC/leer_valor_ADC.ino
+++ 1.leer_valor_ADC/leer_valor_ADC/leer_valor_ADC.ino
@@ -1,6 +1,10 @@
-//------------------ PRÁCTICA 1 ------------------//
+//---------------------------------------- PRÁCTICA 1 ----------------------------------------//
 // Ejercicio 1: Leer el valor del ADC
-int y;
+
+const int pinPotenciometro = 2; //Pin del potenciómetro
+int y; //variable de salida re-asignada
+int ValorPotenciometro;
+
 void setup() {
   Serial.begin(115200); //Función que inicia la comunicación serie a 115200 bits por segundo
 }
@@ -8,8 +12,10 @@ void setup() {
 void loop() {
   //Conecto potenciometro al puerto 2 
   //(Se podrían haber usados los puertos: 36, 39, 34, 32, 33, 25, 27, 27,8, 12)
-  int ValorPotenciometro = analogRead(2); //Lee la entrada analógica del puerto 2
-  y = map(ValorPotenciometro, 0, 4095 , 0, 3300);
+  ValorPotenciometro = analogRead(pinPotenciometro); ////Leo entrada analógica del potenciómetro
+  // La función map(): Re-asigna un número de una gama a otra
+  y = map(ValorPotenciometro, 0, 4095 , 0, 3300); //map(value, fromLow, fromHigh, toLow, toHigh)
+  // He ajustado el valor de salida de 0 a 3300mV.
   Serial.println(y); //Imprime los datos en el Monitor Serie
   delay(1000); //tiempo entre lecturas de 1000 ms (1s)
 
